4537ca104ebdc432744272b4a234f590f8a5ace6
UnfinishedBusiness <travis.gillin@gmail.com>
2018-09-25 14:04:28 -0400

    Commit with build cleaned

--- build/release/geometry/clipper.d
+++
@@ -1,4 +0,0 @@
-build/release/geometry/clipper.o: src/geometry/clipper.cpp \
- src/geometry/clipper.h
-
-src/geometry/clipper.h:
--- build/release/lv_drivers/indev/evdev.d
+++
@@ -1,8 +0,0 @@
-build/release/lv_drivers/indev/evdev.o: src/lv_drivers/indev/evdev.c \
- src/lv_drivers/indev/evdev.h src/lv_drv_conf.h src/lv_conf.h
-
-src/lv_drivers/indev/evdev.h:
-
-src/lv_drv_conf.h:
-
-src/lv_conf.h:
